daa04605b21c7d0f1250ee431f8a9a10ac69471f
Fabo xw <faboGG.12@gmail.com>
2020-09-23 11:32:07 -0300

    terminando

---
+++ Practica 1/Add1.pas
@@ -0,0 +1,10 @@
+{ Realizar un programa que lea 3 números enteros y los imprima en orden descendente. Por ejemplo:
+- Si se ingresan los valores: 4, -10 y 12; deberá imprimir: 12 4 -10}
+program EjerciciosAdd;
+
+var ordenar:integer;
+
+begin
+    ordenar:=0;
+    write('Hola mundo');
+end.
