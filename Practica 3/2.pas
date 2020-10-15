f0ff8eca3d2883d0646b8b491549973972110361
Fabo xw <faboGG.12@gmail.com>
2020-10-14 22:50:13 -0300

    Holo

---
+++ Practica 3/2.pas
@@ -0,0 +1,10 @@
+{. Completar el programa principal para que lea información de alumnos (código, nombre, promedio) e informe la
+cantidad de alumnos leídos. La lectura finaliza cuando ingresa un alumno con código 0, que no debe procesarse.
+Nota: utilizar el módulo leer.
+b. Modificar al programa anterior para que, al finalizar la lectura de todos los alumnos, se informe también el nombre
+del alumno con mejor promedio.}
+program Registros;
+
+begin
+….
+end.
