6742a052ca59afd9c38b6595247a4d916296f641
Fabo xw <faboGG.12@gmail.com>
2020-11-15 20:57:01 -0300

    cosa

--- Practica 6 listas/programas/6.pas
+++ Practica 6 listas/programas/6.pas
@@ -1,6 +1,4 @@
-{6. La Agencia Espacial Europea (ESA) está realizando un relevamiento de todas las sondas espaciales lanzadas al
-espacio en la última década. De cada sonda se conoce su nombre, duración estimada de la misión (cantidad de
-meses que permanecerá activa), el costo de construcción, el costo de mantenimiento mensual y rango del espectro
+{6. La Agencia Espacial Europea (ESA) está realizando un relevamiento de todas las sondas espaciales lanzadas al espacio en la última década. De cada sonda se conoce su nombre, duración estimada de la misión (cantidad de meses que permanecerá activa), el costo de construcción, el costo de mantenimiento mensual y rango del espectro
 electromagnético sobre el que realizará estudios. Dicho rango se divide en 7 categorías:
 1. radio; 2. microondas; 3.infrarrojo; 4. luz visible; 5. ultravioleta; 6. rayos X; 7. rayos gamma
 Realizar un programa que lea y almacene la información de todas las sondas espaciales. La lectura finaliza al ingresar la sonda llamada “GAIA”, que debe procesarse.
