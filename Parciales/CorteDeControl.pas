81ad449f19f30391026ff8f411218c6d3a2f6706
FabianMartinez1234567 <fabianmartinezrincon.123@gmail.com>
2020-12-08 14:46:28 -0300

    e

--- Parciales/CorteDeControl.pas
+++ Parciales/CorteDeControl.pas
@@ -9,7 +9,27 @@ type
 //__________________________________________
 procedure InsertarOrdenado(var L:Lista;numero:integer);
 var
-    ant,act,nue:Lista
+    ant,act,nue:Lista;
+begin
+    new(nue);
+    nue^.datos:=numero;
+    act:=L;
+    ant:=L;
+    while (act <> Nil) and (act^.datos<numero)do
+    begin
+        ant:=act;
+        act:=act^.sig;
+    end;
+    if (ant = act) then
+    begin
+        L:=nue;
+    end
+    else 
+        begin
+            ant^.sig:=nue;
+        end;
+    nue^.sig:=act;
+end;
 //__________________________________________
 procedure CargarLista(var L:Lista);
 var
@@ -24,9 +44,39 @@ begin
     
 end;
 //__________________________________________
+procedure ImprimirLista (L:Lista);
+begin
+    while L <> nil do
+    begin
+        WriteLn('nro: ',L^.datos);
+        L:=L^.sig;
+    end;
+end;
+//__________________________________________
+procedure Problemas(L:Lista);
+var
+    contador:integer;
+    actual:integer;
+begin
+    while L <> nil do
+    begin
+        actual:=L^.datos;
+        contador:=0;
+        while (L <> nil)and (actual=L^.datos) do
+        begin
+            contador:=contador+1;
+            L:=L^.sig;
+        end;
+        
+        WriteLn('El numero:: ',actual,'Nro: ',contador );
+    end;
+end;
+//__________________________________________
 var
     L:Lista;
 begin
     L:=Nil;
     CargarLista(L);
+    Problemas(L);
+    //ImprimirLista(L);
 end.
