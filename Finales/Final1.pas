2f7fa7a77d0a62f93da016f1e210758211265531
FabianMartinez1234567 <fabianmartinezrincon.123@gmail.com>
2021-02-19 13:03:36 -0300

    hi

--- Finales/Final1.pas
+++ Finales/Final1.pas
@@ -1,13 +1,44 @@
 program hola;
 type
-    
-var
-    i:integer;
-begin
-    
-    i:=0;
-    for i:=0 to 5 do 
-    begin
-        writeln('Hola pto');
+    tipo = 1..12;
+    cadena20 = string[20];
+    publicacion = record
+        titulo : cadena20;
+        nombre : cadena20;
+        dni : integer;
+        tipoPublic : tipo;
     end;
+    Vector_public = Array [tipo] of integer;
+//____________________________________________________
+procedure LeerPubli(var P: publicacion);
+begin
+    WriteLn('DNI: ');    ReadLn(P.dni);
+    if (P.dni <> 0) then
+    begin
+        WriteLn('Titulo: ');    ReadLn(P.titulo);
+        WriteLn('Nombre: ');    ReadLn(P.nombre);
+        WriteLn('Tipo: ');    ReadLn(P.tipoPublic);
+    end;
+end;
+//____________________________________________________
+procedure LeerPublicaciones(var vc:Vector_public);
+var
+    P:publicacion;
+    actual: integer;
+begin
+    LeerPubli(P);
+    while (P.dni <> 0) do
+    begin
+        actual = 
+        vc[P.tipoPublic]:=vc[P.tipoPublic]+1;
+
+        LeerPubli(P);
+    end;
+end;
+//____________________________________________________
+var
+    vcontador : Vector_public;
+begin
+    LeerPublicaciones(vcontador);
+    
 end.
