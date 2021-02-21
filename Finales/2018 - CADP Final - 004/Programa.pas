9a585784ecb3b9e9cd3a17af3440b18bba08f77d
FabianMartinez1234567 <fabianmartinezrincon.123@gmail.com>
2021-02-21 16:02:50 -0300

    hi

---
+++ Finales/2018 - CADP Final - 004/Programa.pas
@@ -0,0 +1,14 @@
+program finalCADP;
+type
+    rango = 1..300;    
+    cadena20 = string[20];
+    venta = record
+        nroProd : rango;
+        cantVendida: integer;
+        nomProducto:cadena20;
+
+    end;
+    precios = array [rango] of integer;
+begin
+  
+end.
